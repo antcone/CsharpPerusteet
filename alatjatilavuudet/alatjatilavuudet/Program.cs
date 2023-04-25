2e1d99d9aa92858ee8876fe765fa09ab57c7d7fa
antcone <maiju.siltaniemi@gmail.com>
2023-04-25 17:18:53 +0300

    Summary(required)

--- alatjatilavuudet/alatjatilavuudet/Program.cs
+++ alatjatilavuudet/alatjatilavuudet/Program.cs
@@ -10,12 +10,23 @@ namespace alatjatilavuudet
     {
         static void Main(string[] args)
         {
-
+            // C# Funktiot: Pinta-alat ja tilavuudet (Yhdessä päivä 10)
+            // https://moodle.sakky.fi/mod/assign/view.php?id=1159681
+            // Lasketaan suorakulmion ("neliön") ja kolmion ala sekä piiri
 
             Square squareOne = new Square(5, 10);
-            Console.WriteLine($"Area of square one is {squareOne.Area()}");
+            Console.WriteLine($"Area of square one is {squareOne.Area()}");  // pinta-ala
+            Console.WriteLine($"Circumference of square one is {squareOne.Circumference()}"); //piiri
+
+            Square squareTwo = new Square(1, 2);
+
             Console.WriteLine();
 
+            Triangle triangleOne = new Triangle(2, 5);
+            Console.WriteLine($"Area of triangle one is {triangleOne.Area()}");  // pinta-ala
+            Console.WriteLine($"Perimeter of triangle one is {triangleOne.Perimeter()}");  // pinta-ala
+
+            Console.WriteLine();
 
             Console.ReadKey();
         }
@@ -24,7 +35,9 @@ namespace alatjatilavuudet
     }
 
 
-    public class Square
+    public class Square  // oikeesti tän nimi pitäis olla Rectangle. Neliö on vain suorakulmion erikoistapaus.
+                         // Jos haluttais laskea pelkästään neliön alaa ja piiriä,
+                         // se olis vielä helpompaa kuin mitä tässä on tehty.
     {
         //Properties
         public double Width { get; set; }
@@ -37,7 +50,6 @@ namespace alatjatilavuudet
         {
             Width = width;
             Heigth = heigth;
-
         }
         //Methods
 
---
+++ alatjatilavuudet/alatjatilavuudet/Triangle.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace alatjatilavuudet
+{
+    class Triangle
+    {
+        //Properties
+        public double Heigth { get; set; }
+
+        public double Base { get; set; }
+
+        //Constructors
+        public Triangle(double heigth, double baseParameter)
+        {
+            Heigth = heigth;
+            Base = baseParameter;
+        }
+
+        //Methods
+        // Ei tarvitse static, koska tätä käytetään luokan objektista.
+        // Esim. Triangle test = new Triangle(2, 5); - Luodaan objekti luokasta nimeltä "test"
+        // Console.WriteLine(test.Area());   - käytetään objektin metodia
+        public double Area()
+        {
+            double result = 0;
+
+            result = Base * Heigth / 2;
+
+            return result;
+        }
+        
+        public double Perimeter()  //Math.Exp() laittaa matemaattisen vakion e annettuun potenssiin, 
+                                    // eli ei tarvita tässä sitä vaan Math.Pow(luku, potenssi)
+        {
+            // hypotenuse = √(base^2 + heigth^2)
+
+            double result = 0;
+
+            result = Heigth + Base + Math.Sqrt(Math.Pow(Base, 2) + Math.Pow(Heigth, 2));
+
+            return result;
+        }
+
+
+
+    }
+}
