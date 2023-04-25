602874ba70038cdceaa55b2bb92b2dc53f7b8ed1
antcone <maiju.siltaniemi@gmail.com>
2023-04-25 19:03:51 +0300

    ..-

---
+++ bensankulutus_luokkainkera/bensankulutus_luokkainkera/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bensankulutus_luokkainkera
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //
+
+
+
+            //Pyydetään arvot käyttäjältä
+
+            List<Trip> trips = new List<Trip>();
+
+            bool userEndsLoop = false;
+
+            while (userEndsLoop == false)
+            {
+                Console.Write("Syötä matkan nimi: ");
+                string name = Console.ReadLine();
+
+                Console.Write("Syötä matka (km): ");
+                double distance = double.Parse(Console.ReadLine());
+
+                Console.Write("Syötä keskikulutus (l/100km): ");
+                double averageConsumption = double.Parse(Console.ReadLine());
+
+                Console.Write("Syötä polttoaineen hinta (€/l): ");
+                double fuelPrice = double.Parse(Console.ReadLine())
+;
+                trips.Add(new Trip(name, distance, averageConsumption, fuelPrice));
+
+                Console.Write("Syötetään uusi matka (y): ");
+                string input = Console.ReadLine();
+
+                if (input != "y")
+                {
+                    userEndsLoop = true;
+                }
+            }
+
+            foreach (Trip t in trips)
+            {
+                // Laskutoimituksen voisi toteuttaa täällä mainissa käyttämällä objektien arvoja,
+                // mutta yleensä luokan dataan liittyvät toiminnallisuudet 
+                // ovat osana itse luokkaa.
+                // double test = t.AverageConsumption * t.FuelPrice;
+                Console.WriteLine($"Matkan {t.Name} hinta on: {t.CalculateCost()} euroa");
+            }
+
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ bensankulutus_luokkainkera/bensankulutus_luokkainkera/Trip.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bensankulutus_luokkainkera
+{
+    class Trip
+    {
+        // Properties  
+        // Data class, luokkaa voi käyttää pelkästään tietorakenteena, ilman toimintoja.
+
+        public string Name { get; set; }
+
+        public double Distance { get; set; }
+
+        public double AverageConsumption { get; set; }
+
+        public double FuelPrice { get; set; }
+
+        private double  privateProperty { get; set; }  //Privaatti properties pienellä alkukirjaimella kuulemma, t. Microsoft
+
+        // Constructors
+        public Trip(string name, double distance, double averageConsumption, double fuelPrice)
+        {
+            Name = name;   //tallennetaan paremetri propertiin
+            Distance = distance;
+            AverageConsumption = averageConsumption;
+            FuelPrice = fuelPrice;
+        }
+
+        // Methods
+
+        //lasketaan matkan hinta mieluummin täällä ku mainissa
+        //tää ei nyt sit oo pelkkä data class
+        public double CalculateCost()
+        {
+            return Distance * AverageConsumption * FuelPrice / 100;
+        }
+
+
+
+
+
+    }
+}
