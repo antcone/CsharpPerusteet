403e4eb569c95fd3dd1e34962f26a1cdf75dd642
antcone <maiju.siltaniemi@gmail.com>
2023-05-12 19:56:55 +0300

    .--..-.

---
+++ wannabevandal/wannabevandal/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wannabevandal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Sticker sticky1 = new Sticker("Able", "zombiehead", 2006, "green", false);
+            Sticker sticky2 = new Sticker("Vepa", "pincushion", 2012, "silver", true);
+            Sticker sticky3 = new Sticker("Pöölau", "reading_owl", 2021, "pink", false);
+            Sticker sticky4 = new Sticker("Able", "able_seaman", 2011, "blue", false);
+            Sticker sticky5 = new Sticker();
+
+
+            Console.WriteLine($"Tarra on {sticky1.HowOldIsIt()} vuotta vanha");
+
+            //Tehhää lista tarroja varten
+            List<Sticker> stickers = new List<Sticker>();
+
+
+            //Luodaan niitä tarroja
+            while(true)
+            {
+
+
+            Console.WriteLine("Anna tekijän nimi: (tai kirjoita 'end')" );
+            string artist = Console.ReadLine();
+                if (artist.ToLower() == "end")
+                {
+                    break;
+                }
+
+                Console.WriteLine("Anna tarran nimi: ");
+            string stickerName = Console.ReadLine();
+
+            Console.WriteLine("Anna tarran tekovuosi: ");
+            int year = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Anna tarran pääväri: ");
+            string colour  = Console.ReadLine();
+
+            Console.WriteLine("Onko tarra foili? K tai E ");
+            char choice = char.Parse(Console.ReadLine());
+            bool foil = choice == 'k' || choice == 'K' ? true : false;
+
+
+                stickers.Add(new Sticker(artist, stickerName, year, colour, foil));
+
+
+            }
+
+
+            foreach (Sticker sticker in stickers)
+            {
+                //onpas tässä hirveän näppärä ternary, melkein tajunnanräjäyttävä. En tietty keksiny itte, vaan chatGPT ehdotti
+                Console.WriteLine($"Tarran {sticker.StickerName} on tehnyt {sticker.ArtistName} vuonna {sticker.CreationYear}. Sen pääväri on {sticker.MainColour} ja {(sticker.Foil ? "se on" : "se ei ole")} foili.");
+                Console.WriteLine($"Tarra on {sticker.HowOldIsIt()} vuotta vanha.");
+            }
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ wannabevandal/wannabevandal/Sticker.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wannabevandal
+{
+    class Sticker
+    {
+        //properties
+
+        public string StickerName { get; set; }
+        public string ArtistName { get; set; }
+        public int CreationYear { get; set; }
+        public string MainColour { get; set; }
+        public bool Foil { get; set; }
+        
+
+        //constructors
+
+
+        public Sticker (string sticker, string artist, int year, string colour, bool foil)
+        {
+
+            StickerName = sticker;
+            ArtistName = artist;
+            CreationYear = year;
+            MainColour = colour;
+            Foil = foil;
+
+        }
+
+        public Sticker()
+        {
+
+            //chatGPT osas kertoa, että tarvitsen tämän
+            Foil = false;
+        }
+
+
+        //methods
+
+        public int HowOldIsIt()
+
+        {
+            return @DateTime.Now.Year - CreationYear;
+        }
+
+
+
+
+
+    }
+}
