7bdd10a4bbedc3d9c8798d35b8410b73bae6e67e
antcone <129174152+antcone@users.noreply.github.com>
2023-05-15 02:34:27 +0300

    ...

--- wannabevandal/wannabevandal/Program.cs
+++ wannabevandal/wannabevandal/Program.cs
@@ -11,17 +11,32 @@ namespace wannabevandal
         static void Main(string[] args)
         {
 
-            Sticker sticky1 = new Sticker("Able", "zombiehead", 2006, "green", false);
-            Sticker sticky2 = new Sticker("Vepa", "pincushion", 2012, "silver", true);
-            Sticker sticky3 = new Sticker("Pöölau", "reading_owl", 2021, "pink", false);
-            Sticker sticky4 = new Sticker("Able", "able_seaman", 2011, "blue", false);
+            Sticker sticky1 = new Sticker("zombiehead", "Able",  2006, "green", false);
+            Sticker sticky2 = new Sticker("pincushion", "Vepa", 2012, "silver", true);
+            Sticker sticky3 = new Sticker("reading_owl", "Pöölau",  2021, "pink", false);
+            Sticker sticky4 = new Sticker("able_seaman", "Able",  2011, "blue", false);
             Sticker sticky5 = new Sticker();
+
+            //no mä loin noita tarroja, mut haluan ne taulukkoon
+            
+            Sticker[] oldStickers = {sticky1, sticky2, sticky3, sticky4};
+
+            for (int i = 0; i < oldStickers.Length; i++)
+
+            {
+                Console.WriteLine(oldStickers[i].StickerName);
+            }
+
+
 
 
             Console.WriteLine($"Tarra on {sticky1.HowOldIsIt()} vuotta vanha");
 
             //Tehhää lista tarroja varten
             List<Sticker> stickers = new List<Sticker>();
+
+            //lisätään aiemman taulukon tavarat tänne
+            stickers.AddRange(oldStickers);
 
 
             //Luodaan niitä tarroja
@@ -50,7 +65,7 @@ namespace wannabevandal
             bool foil = choice == 'k' || choice == 'K' ? true : false;
 
 
-                stickers.Add(new Sticker(artist, stickerName, year, colour, foil));
+                stickers.Add(new Sticker(stickerName, artist, year, colour, foil));
 
 
             }
