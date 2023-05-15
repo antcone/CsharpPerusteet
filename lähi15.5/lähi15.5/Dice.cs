6fc78b1237d730c30a3a49a24faffc41404a74ec
antcone <maiju.siltaniemi@gmail.com>
2023-05-15 17:34:07 +0300

    .-..

---
+++ lähi15.5/lähi15.5/Dice.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lähi15._5
+{
+    class Dice
+    {
+        // Properties
+
+        private Random rng { get; set; }  // jos luodaan static. tämä jaetaan kaikkien noppaobjektien kesken.
+                                          // Silloin ei saada enää samoja arvoja
+
+        public int MaxValue { get; set; } // Nopassa on lukuja 1 - tämä arvo
+
+        public int Value { get; set; }  // Nopan arvo, joka on ylöspäin (silmäluku)
+
+        // Jokin property, jolla voi tehdä "huijaus"nopan
+
+        // Constructors
+
+        public Dice(int maxValue)
+        {
+            rng = new Random();
+            MaxValue = maxValue;
+            Roll();
+        }
+
+        public Dice(int maxValue, int value)  //Kahden parametrin overload
+        {
+            rng = new Random();
+            MaxValue = maxValue;
+            Value = value;
+            
+        }
+
+        // Methods
+        
+        // Noppaa heitetään ja arvotaan siihen uusi satunnainen arvo
+        public void Roll()
+        {
+            
+            Value = rng.Next(1, MaxValue + 1);
+        }
+
+
+
+    }
+}
---
+++ lähi15.5/lähi15.5/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lähi15._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Dice noppa1 = new Dice(6);
+            Dice noppa2 = new Dice(6, 1);
+
+            noppa1.Roll();
+
+            noppa2.Roll();
+
+            Console.WriteLine($"Nopan 1 silmäluku on {noppa1.Value}");
+            Console.WriteLine($"Nopan 2 silmäluku on {noppa2.Value}");
+
+            Console.WriteLine();
+
+            noppa1.Roll();
+
+            noppa2.Roll();
+
+            Console.WriteLine($"Nopan 1 silmäluku on {noppa1.Value}");
+            Console.WriteLine($"Nopan 2 silmäluku on {noppa2.Value}");
+
+            Console.WriteLine();
+
+            noppa1.Roll();
+
+            noppa2.Roll();
+
+            Console.WriteLine($"Nopan 1 silmäluku on {noppa1.Value}");
+            Console.WriteLine($"Nopan 2 silmäluku on {noppa2.Value}");
+
+            Console.WriteLine();
+
+            noppa1.Roll();
+
+            noppa2.Roll();
+
+            Console.WriteLine($"Nopan 1 silmäluku on {noppa1.Value}");
+            Console.WriteLine($"Nopan 2 silmäluku on {noppa2.Value}");
+
+            Console.WriteLine();
+
+            noppa1.Roll();
+
+            noppa2.Roll();
+
+
+
+
+
+            Console.WriteLine($"Nopan 1 silmäluku on {noppa1.Value}");
+            Console.WriteLine($"Nopan 2 silmäluku on {noppa2.Value}");
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
